5ba669d42653b16f18f3f6aba0631195054cf4be
huyfr <61008601+huyfr@users.noreply.github.com>
2020-03-02 08:58:42 +0700

    Sửa bài tập chuyển đổi giữa feet và meters

--- convertUnits.js
+++ convertUnits.js
@@ -1,13 +1,9 @@
 let result;
-function feetToMeters(number)
+let units;
+function convertUnits(number)
 {
+    units=document.getElementById("units").value;
     number=document.getElementById("feet").value;
-    result=number*0.305;
+    result=number*units;
     document.getElementById("resultMeters").innerHTML=result+" meters";
-}
-function metersToFeet(number)
-{
-    number=document.getElementById("meters").value;
-    result=number*3.279;
-    document.getElementById("resultFeet").innerHTML=result+" feet";
 }
